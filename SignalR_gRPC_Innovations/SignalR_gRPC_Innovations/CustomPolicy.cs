f313238716d6f54673c3d4e36a151b8411d1e825
Rufat <rufat@Rufats-MacBook-Air.local>
2024-04-14 00:43:31 +0400

    feat:I added experience codes to my gitHub repo

---
+++ SignalR_gRPC_Innovations/SignalR_gRPC_Innovations/CustomPolicy.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using Microsoft.AspNetCore.RateLimiting;
+using System.Threading.RateLimiting;
+
+namespace SignalR_gRPC_Innovations
+{
+    public class CustomPolicy : IRateLimiterPolicy<string>
+    {
+        public CustomPolicy()
+        {
+        }
+
+        public Func<OnRejectedContext, CancellationToken, ValueTask>? OnRejected => (context, cancellationToken) =>
+        {
+            return new();
+        };
+
+        public RateLimitPartition<string> GetPartition(HttpContext httpContext)
+        {
+            return RateLimitPartition.GetFixedWindowLimiter("", _ => new()
+            {
+                PermitLimit = 4,
+                Window = TimeSpan.FromMinutes(20),
+                QueueLimit = 2,
+                QueueProcessingOrder = QueueProcessingOrder.OldestFirst,
+
+            });
+        }
+    }
+}
+
---
+++ SignalR_gRPC_Innovations/SignalR_gRPC_Innovations/ServiceRegistration.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Threading.RateLimiting;
+using Microsoft.AspNetCore.RateLimiting;
+
+namespace SignalR_gRPC_Innovations
+{
+    public static class ServiceRegistration
+    {
+        public static void Registration(this IServiceCollection services, IConfiguration configuration)
+        {
+            services.AddControllers();
+            // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+            services.AddEndpointsApiExplorer();
+            services.AddSwaggerGen();
+            #region Fiex Window Algoritms
+            services.AddRateLimiter(options =>
+            {
+                options.AddFixedWindowLimiter("Fixed", _options =>
+                {
+                    _options.Window = TimeSpan.FromSeconds(10);
+                    _options.PermitLimit = 4;
+                    _options.QueueLimit = 3;
+                    _options.QueueProcessingOrder = QueueProcessingOrder.OldestFirst;
+                });
+                /*
+                10 saniyədə 10 sorğuya cavab verə biləcək və extra sorğulardan 3 nü gözləməyə alacaq
+                digərlərini görməzdən gələcəy və 10 sorğunu qarşıladıqdan sonra gözləməyə alıqı 3 sorğunu köhnədən yeniyə
+                doğru qarşılayacaq.
+                */
+            });
+
+            #endregion
+            #region Sliding Window
+            services.AddRateLimiter(options =>
+            {
+                options.AddSlidingWindowLimiter("Sliding", _option =>
+                {
+                    _option.Window = TimeSpan.FromSeconds(10);
+                    _option.PermitLimit = 4;
+                    _option.QueueLimit = 2;
+                    _option.QueueProcessingOrder = QueueProcessingOrder.OldestFirst;
+                    _option.SegmentsPerWindow = 2;//Bunun vasitəsi ilə 5 ci saniyədən sonra 4 limiti bitsə 2 sinidə extra işlədə biləcək.
+                });
+            });
+            #endregion
+            #region Token Bucket
+            services.AddRateLimiter(options =>
+            {
+                options.AddTokenBucketLimiter("Token", _options =>
+                {
+                    _options.QueueLimit = 2;
+                    _options.QueueProcessingOrder = QueueProcessingOrder.OldestFirst;
+                    _options.TokenLimit = 4;
+                    _options.TokensPerPeriod = 4;
+                });
+            });
+            #endregion
+            #region Concruency
+            services.AddRateLimiter(options =>
+            {
+                options.AddConcurrencyLimiter("Concurency", _options =>
+                {
+                    _options.PermitLimit = 4;
+                    _options.QueueLimit = 2;
+                    _options.QueueProcessingOrder = QueueProcessingOrder.OldestFirst;
+                });
+                options.OnRejected = (context, cancellationToken) =>
+                {
+                    return new();
+                };
+            });
+            //paralel olaraq 4 request baş verə bilər eyni zamanda və 2 sini gözləməyə alacaq 4 ü bitəndən sonra o 2 sini başladacaq və ondan elave gələn requestləri almayacaq.
+            #endregion
+            #region CustomPolicy
+            services.AddRateLimiter(options =>
+            {
+                options.AddPolicy<string, CustomPolicy>("CustomPolicy");
+            });
+            #endregion
+
+        }
+    }
+}
+
